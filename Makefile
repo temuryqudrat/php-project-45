c654191f3a5b91f5e49dcee02442193d271fe56a
temuryqudrat <qudrat.temury@gmail.com>
2026-06-28 20:47:56 +0300

    Add greeting function

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ install:
 	php C:\xampp\php\composer.phar install
 
 brain-games:
-	./bin/brain-games
+	"C:/xampp/php/php.exe" bin/brain-games
 
 validate:
 	php C:\xampp\php\composer.phar validate
---
+++ src/Cli.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace BrainGames\Cli;
+
+function greeting()
+{
+    echo "Welcome to the Brain Games!\n";
+    $name = \cli\prompt('May I have your name?', false, ' ');
+    echo "Hello, {$name}!\n";
+}
